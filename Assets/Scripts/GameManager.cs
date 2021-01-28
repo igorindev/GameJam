561d94bdf98bc1aa48325c3a96fade7698412af9
igorindev <igorindev@users.noreply.github.com>
2021-01-27 21:15:46 -0300

    Duração

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] Delivery[] allGameItens;
     [SerializeField] List<Delivery> allGameSpawned;
+    [SerializeField] public int minutesDuration;
 
     public Delivery[] AllGameItens { get => allGameItens; set => allGameItens = value; }
     public List<Delivery> AllGameSpawned { get => allGameSpawned; set => allGameSpawned = value; }
+
+    int minutes;
+    int seconds;
+    float startTime;
 
     void Awake()
     {
@@ -23,15 +30,39 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
 
+        minutes = minutesDuration;
     }
 
     void Update()
     {
-        
+        TimerUpdate();
     }
 
-    void Timer()
+    public void TimerUpdate(float delayTime = 1)
     {
+        if (minutes >= 0)
+        {
+            if (Time.time - startTime > delayTime)
+            {
+                startTime = Time.time;
 
+                if (seconds <= 0)
+                {
+                    seconds = 59;
+                    minutes -= 1;
+                }
+                else
+                {
+                    seconds -= 1;
+                }
+            }
+
+            timerText.text = $"{minutes}:{seconds}";
+        }
+        else
+        {
+            Debug.Log("Time up");
+        }
     }
+
 }
