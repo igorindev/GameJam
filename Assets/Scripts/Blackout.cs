f3cebffbc13a8d24cfb3fb903ad9961ff7587832
igorindev <igorindev@users.noreply.github.com>
2021-01-28 17:00:41 -0300

    Correção

---
+++ Assets/Scripts/Blackout.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackout : MonoBehaviour
+{
+    [SerializeField] Vector2 timeToNextBlackout;
+    [SerializeField] Light[] lights;
+
+    private void Start()
+    {
+        RuffleDelayTime();
+    }
+
+    public void RuffleDelayTime()
+    {
+        float delay = Random.Range(timeToNextBlackout.x, timeToNextBlackout.y);
+
+        StartCoroutine(Delay(delay));
+    }
+
+    IEnumerator Delay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+    }
+}
